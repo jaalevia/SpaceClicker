c5e450ccb15520b3ba6c92818f0d7d92c63efbd3
Alexandra <liysan@yandex.com>
2022-06-01 15:50:21 +0300

    PlayerStatus
    Health bar and death system added

--- SpaceClicker/Assets/Scripts/PlayerScript.cs
+++ SpaceClicker/Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     public GameObject monster;
     public GameObject player;
     public GameObject deathPanel;
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+    public HealthScript healthBar;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (player == gameObject)
         {
-            deathPanel.SetActive(true);
+            TakeDamage(25);
+            if (currentHealth == 0)
+            {
+                deathPanel.SetActive(true);
+            }
+            
             //Debug.Log(collision.name);
         }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
